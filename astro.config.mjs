28c06ca58977bf6b59ab8086f34187040758696a
jw <choi.jae100@gmail.com>
2025-08-12 23:31:14 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import catppuccin from "@catppuccin/starlight";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://jwc20.github.io",
   base: "bnc-docs",
